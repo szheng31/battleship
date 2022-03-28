461f63cefd4867930d137b3863f4a86e3e042de5
Subaah <101998607+subaah@users.noreply.github.com>
2022-03-27 22:33:27 -0400

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -41,10 +41,10 @@ public abstract class Ship {
     }
 
 	public boolean okToPlaceShipAt(int row, int column, boolean horizontal, Ocean ocean) {
-		/*Returns true if it is okay to put a ship of this length with its bow in this location, with the given orientation, and returns false otherwise. 
-		  The ship must not overlap another ship, or touch another ship (vertically, horizontally, or diagonally), and it must not "stick out" beyond the 
-		  array. Does not actually change either the ship or the Ocean, just says whether it is legal to do so. Uses the inBound method from Ocean.java.
-		*/
+	/*Returns true if it is okay to put a ship of this length with its bow in this location, with the given orientation, and returns false otherwise. 
+	  The ship must not overlap another ship, or touch another ship (vertically, horizontally, or diagonally), and it must not "stick out" beyond the 
+	  array. Does not actually change either the ship or the Ocean, just says whether it is legal to do so. Uses the inBound method from Ocean.java.
+	  */
 		if (!inBound(row,column)) return false;
 		// if tiles are occupied
 		if (ocean.isOccupied(row,column)) return false;
@@ -83,6 +83,9 @@ public abstract class Ship {
 
 	//assumes it is safe to place ship at row, col
 	public void placeShipAt(int row, int column, boolean horizontal, Ocean ocean) {
+	/* "Puts" the ship in the ocean. This involves giving values to the bowRow, bowColumn, and horizontal instance variables in the ship,
+	   and it also involves putting a reference to the ship in each of 1 or more locations (up to 4) in the ships array in the Ocean object.
+	   */
 		if (okToPlaceShipAt(row,column,horizontal,ocean)) {
 			setBowRow(row);
 			setBowColumn(column);
@@ -106,6 +109,9 @@ public abstract class Ship {
 	}
 
 	public boolean shootAt(int row, int column) {
+	/*If a part of the ship occupies the given row and column, and the ship hasn't already been sunk, mark that part of the ship 
+	  as "hit" and return true, otherwise return false.
+	  */
 		if (!isSunk()) {
 			if (horizontal) {
 				if (row == getBowRow() && column < getBowColumn() + length) {
@@ -125,7 +131,8 @@ public abstract class Ship {
 
 	}
 
-	public boolean isSunk() {
+	public boolean isSunk() 
+	//Return true if every part of the ship has been hit, false otherwise.
 		for (int i = 0; i < length; i++) {
 			if (hit[i] == false) return false;
 		}
@@ -134,6 +141,7 @@ public abstract class Ship {
 
 	@Override
 	public String toString() {
+	//This method returns "x" if the ship has been sunk, "S" if it has not been sunk. 
 		if (this.isSunk()) {
 			return "x";
 		}
